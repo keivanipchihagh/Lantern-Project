7bb755c53377f1b485226b6f00e0a257b5c2d06e
Keivan Ipchi Hagh <ipchi1380@gmail.com>
2021-02-12 21:10:16 +0330

    Initial channel configurations

--- LanternProject/api/views.py
+++ LanternProject/api/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello World.')
+    return render(request, 'api/index.html')
