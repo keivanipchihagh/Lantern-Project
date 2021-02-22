e6fa5bb1b85bffe5675ca376491ad15fb01f08ba
Keivan Ipchi Hagh <ipchi1380@gmail.com>
2021-02-22 10:54:13 +0330

    Update script.js

--- LanternProject/dashboard/static/dashboard/scripts/script.js
+++ LanternProject/dashboard/static/dashboard/scripts/script.js
@@ -1,17 +1,205 @@
-var user_key = '123456789'
+const Profile = new Vue({
 
-// ----------------------------------------------------------- Profile ----------------------------------------------------------------------
+    el: "#profile",    
 
+    data: {
+        user_key: '123456789'
+    },
 
+    methods: {
+    },
 
-// ----------------------------------------------------------- Profile ----------------------------------------------------------------------
+    mounted: function () {
+        self = this
 
-$('form#form_pi').submit(function(e) {
-    $.ajax({
-        url: 'http://127.0.0.1:8000/dashboard/v1/profile/' + user_key + '/update/pi',
-        type: $(this).attr('method'),
-        data: $(this).serialize(),
-        error: function(){$("#submit_btn").val('Oops! Somthing went wrong').attr('class', 'btn btn-sm white m-b danger').prop('disabled', true) },
-        success: function (response) { $("#submit_btn").val(response).attr('class', (response == 'Updated!' ? 'btn btn-sm white m-b success' : 'btn white m-b warn')) },
-    });
+        $('form#form_pi').submit(function(e) {
+            $.ajax({
+                url: 'http://127.0.0.1:8000/dashboard/v1/profile/' + self.user_key + '/update/pi',
+                type: $(this).attr('method'),
+                data: $(this).serialize(),
+                error: function(){$("#submit_btn").val('Oops! Somthing went wrong').attr('class', 'btn white m-b danger').prop('disabled', true) },
+                success: function (response) { $("#submit_btn").val(response).attr('class', (response == 'Updated!' ? 'btn white m-b success' : 'btn white m-b warn')) },
+            });
+        })
+
+        
+    },
+
+    watch: {
+        
+    },
+
+    components: {        
+    }
+})
+
+const ChatApp = new Vue({
+
+    el: "#app",
+
+    data: {
+        packages: [],
+        message: '',
+        user_key: '123456789',
+    },
+
+    methods: {
+
+        sendMessage: function () {            
+            document.querySelector('#chat-message-submit').click()
+        },        
+        openChat: function (session_key) {
+
+            document.querySelector('#chatTitle').innerText = 'Chat - ' + session_key
+            const chatSocket = new WebSocket('ws://' + window.location.host + '/ws/session/' + session_key + '/');
+            self = this
+            
+            chatSocket.onmessage = function (e) {
+                const data = JSON.parse(e.data)
+
+                var package = { id: data['id'], message: data['message'], datetime: data['datetime'], sender: 'client' }
+
+                // Push agent packages only
+                if (!ChatApp.messageExists(package['id'])) ChatApp.packages.push(package)
+            }
+            
+            document.querySelector('#chat-message-submit').onclick = function (e) {
+                const message = self.message                
+
+                var package = { id: ChatApp.packages.length, message: message.replace(/\n/g, ''), datetime: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }), sender: 'agent' }
+                
+                chatSocket.send(JSON.stringify({
+                    'id': package['id'],
+                    'message': package['message'],
+                    'datetime': package['datetime'],
+                    'session_key': session_key,
+                    'sender': package['sender'],
+                }));
+
+                self.message = ''
+                ChatApp.packages.push(package)
+            }
+
+            chatSocket.onclose = function (e) { console.error('Server connection was terminated.') }
+
+            this.getChat(session_key, this.user_key)
+        },
+
+        getChat: function(session_key, user_key) {
+            var self = this            
+
+            $.ajax({
+                url: 'http://127.0.0.1:8000/dashboard/v1/fetch/session',
+                type: 'GET',
+                context: this,      // Essential for VueJS
+                data: {
+                    'session_key': session_key,
+                    'user_key': user_key
+                },
+                error: function () {
+                    console.log('There was a problem fetching messages from server.');
+                },
+                success: function (data) {
+                    var i = 0
+                    self.packages = []
+                    for (i; i < data.length; i++) {
+                        var entry = JSON.parse(data[i].replace(/'/g, '"'))                        
+                        self.packages.push({id: entry['id'], message: entry['content'], datetime: entry['datetime'], sender: entry['sender']})
+                        $("#star").attr('class', (entry['starred'] == 1)  ?'fa fa-star text-info' : ' fa fa-star-o text-info')
+                    }                    
+                },
+            });
+        },
+
+        closeChat: function(session_key) {
+            // Close the session, delete the chats and remove the session from dashboard
+            $.ajax({
+                url: 'http://127.0.0.1:8000/dashboard/v1/close/session',
+                type: 'GET',
+                data: { 'session_key': session_key, 'user_key': this.user_key },
+                error: function () { console.error('Session could not be closed') },
+                success: function () {
+                    console.log('Session Closed.')
+
+                    $("#assigned_" + session_key).remove()
+                    $("#open_" + session_key).remove()
+                    $('#chatTitle').text('Chat')
+                    $("#assigned_sessions_count").text(parseInt($("#assigned_sessions_count").text()) - 1)
+                    $("#open_sessions_count").text(parseInt($("#open_sessions_count").text()) - 1)
+                },
+            });
+        },
+
+        starChat: function(session_key) {
+            // Star the session
+            $.ajax({
+                url: 'http://127.0.0.1:8000/dashboard/v1/star/session',
+                type: 'GET',
+                data: { 'session_key': session_key, 'user_key': this.user_key },
+                error: function () { console.error('Session could not be starred') },
+                success: function () {
+                    console.log('Session Starred.')
+                    $("#star").toggleClass('fa-star-o fa-star')
+                    
+                    if ($("#star").attr("class").includes('fa-star-o')) {
+                        $("#starred_" + session_key).remove()
+                        $("#starred_sessions_count").text(parseInt($("#starred_sessions_count").text()) - 1)
+                    } else {
+                        var starred = $("#assigned_" + session_key).clone(true)
+                        starred.attr({"id": "starred_" + session_key, "class": ""})
+                        
+                        $("#starred_sessions").html($("#starred_sessions").html() + $("<div />").append(starred.clone()).html())                        
+                        $("#starred_sessions_count").text(parseInt($("#starred_sessions_count").text()) + 1)
+                    }
+                },
+            });
+        },
+
+        messageExists: function (id) {
+            for (var i = 0; i < this.packages.length; i++) if (this.packages[i]['id'] == id) return true
+            return false
+        },        
+    },
+
+    mounted: function () {
+        
+    },
+
+    watch: {
+        
+    },
+
+    components: {
+        'Package': {
+            props: ['message', '_sender', 'datetime', '_id'],
+
+            template: `
+                <!-- Agent - start -->
+                <div class="m-b" v-if="this.sender != 'client'">
+                    <a href="" class="pull-right w-40 m-l-sm"><img src="../../../static/dashboard/images/a3.jpg" class="w-full img-circle" alt="..."></a>
+                    <div class="clear text-right">
+                        <div class="p-a p-y-sm info inline text-left r">{{ message }}</div>
+                        <div class="text-muted text-xs m-t-xs">{{ datetime }}</div>
+                    </div>
+                </div>
+                <!-- Agent - end -->
+                
+                <!-- User - start -->
+                <div class="m-b" v-else>
+                <a href="" class="pull-left w-40 m-r-sm"><img src="../../../static/dashboard/images/a3.jpg" class="w-full img-circle" alt="..."></a>
+                    <div class="clear">
+                        <div><div class="p-a p-y-sm dark-white inline r">{{ message }}</div></div>
+                        <div class="text-muted text-xs m-t-xs"><i class="fa fa-ok text-success"></i>{{ datetime }}</div>
+                    </div>
+                </div>
+                <!-- User - end -->`,
+
+            data: function () {
+                return {
+                    sender: this._sender,
+                    id: this._id,
+                }
+            }
+        }
+    }
 })
