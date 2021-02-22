bbbbd87d45ae75c24e24f89e7247a5b74f1c67ec
Keivan Ipchi Hagh <ipchi1380@gmail.com>
2021-02-22 11:17:20 +0330

    Update asgi.py

--- LanternProject/LanternProject/asgi.py
+++ LanternProject/LanternProject/asgi.py
@@ -14,7 +14,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import api.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LanternProject.settings")
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
