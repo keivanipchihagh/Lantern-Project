0cd13780b73ee7662185a3f0a8494b7dbec1b967
Keivan Ipchi Hagh <ipchi1380@gmail.com>
2021-02-26 21:04:53 +0330

    Update settings.py

--- LanternProject/LanternProject/settings.py
+++ LanternProject/LanternProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xxxxx'
+SECRET_KEY = '6-1^ir8_#6c=-==7wpkbbg+e=6gut09e(t73&%b=4%+q@o$w&6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
