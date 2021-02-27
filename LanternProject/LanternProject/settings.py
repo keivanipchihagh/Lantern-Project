aaa401db9e6877e8e998d648b3c66c78bcbee0b4
Keivan Ipchi Hagh <ipchi1380@gmail.com>
2021-02-27 05:14:32 +0330

    Update settings.py

--- LanternProject/LanternProject/settings.py
+++ LanternProject/LanternProject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '6-1^ir8_#6c=-==7wpkbbg+e=6gut09e(t73&%b=4%+q@o$w&6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1'
